d7b538f968742f0b7a7b3914419ab43be40c36c7
Tom Britton <britton.tm@gmail.com>
2025-07-07 23:00:56 -0500

    Static asset handling

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ import (
 func main() {
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", handlers.GetIndex)
+	router.HandleFunc("GET /{$}", handlers.GetIndex)
+	router.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
+	// @TODO 404 handler
+	// router.HandleFunc("/", handlers.Get404) // Catch everything else
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
